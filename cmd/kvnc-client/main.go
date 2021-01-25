d387343c47847390a48f628b06b5ab97d4618a79
Yutaka Kato <mikan@aomikan.org>
2021-01-25 10:44:07 +0900

    macOS support, under testing

--- cmd/kvnc-client/main.go
+++ cmd/kvnc-client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+
 	"fyne.io/fyne/v2"
 )
 
--- cmd/kvnc-client/vnc.go
+++ cmd/kvnc-client/vnc.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"fmt"
-	"github.com/kaginawa/kvnc"
 	"os"
 	"os/exec"
+	"runtime"
 	"strings"
+
+	"github.com/kaginawa/kvnc"
 )
 
 const (
 	hostPlaceholder      = "{HOST}"
 	portPlaceholder      = "{PORT}"
 	defaultCommandFormat = "./vncviewer " + hostPlaceholder + "::" + portPlaceholder
+	macOSCommandFormat   = "open vnc://" + hostPlaceholder + ":" + portPlaceholder
 )
 
 type viewerParams struct {
@@ -22,19 +25,24 @@ type viewerParams struct {
 
 func startViewer(port string, params viewerParams) {
 	format := defaultCommandFormat
+	if runtime.GOOS == "darwin" {
+		format = macOSCommandFormat
+	}
 	if len(config.ViewerCmd) > 0 {
 		format = config.ViewerCmd
 	}
 	command := strings.Replace(format, hostPlaceholder, "localhost", 1)
 	command = strings.Replace(command, portPlaceholder, port, 1)
-	if params.trueColor {
-		command += " -8bit"
-	}
-	if params.fullScreen {
-		command += " -fullscreen"
-	}
-	if params.viewOnly {
-		command += " -viewonly"
+	if runtime.GOOS != "darwin" {
+		if params.trueColor {
+			command += " -8bit"
+		}
+		if params.fullScreen {
+			command += " -fullscreen"
+		}
+		if params.viewOnly {
+			command += " -viewonly"
+		}
 	}
 	tokens := strings.Split(command, " ")
 	viewer := exec.Command(tokens[0], tokens[1:]...)
